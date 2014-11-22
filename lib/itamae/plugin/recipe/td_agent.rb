553e087a0ed96d9ce40fe78d000db94e2ff9d0dc
Takatoshi Maeda <me@tmd.tw>
2014-11-23 06:11:23 +0900

    Initial commit

---
+++ lib/itamae/plugin/recipe/td_agent.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/td_agent/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module TdAgent
+        # Your code goes here...
+      end
+    end
+  end
+end
